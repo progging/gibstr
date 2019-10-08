f6d0ca7e167094d39d80fc98cb12cf9a2a116ba6
robin <robin@pettersen.dev>
2019-10-08 17:12:43 +0200

    Fixed inconsistent behaviour. Added length param.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-export default () => Math.random().toString(36).substring(2)
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+
+/**
+ * Generates an alphanumeric string with the given length.
+ * @param length {Number} Length of the string. 10 by default.
+ * @returns {string}
+ */
+export default (length = 10) => {
+    let str = ""
+    for (var i = 0; i < length; i++ ) {
+        str += characters.charAt(Math.floor(Math.random() * characters.length))
+    }
+    return str
+}
