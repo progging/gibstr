114b9b1ac78c8d279847f12e5258cb49bb91126e
Robin <rob@tenk.dev>
2020-08-18 18:24:41 +0200

    Fixed GH workflow

--- dist/gibstr.d.ts
+++
@@ -1,6 +0,0 @@
-declare const _default: (length?: number) => string;
-/**
- * Generates an alphanumeric string with the given length.
- * @param length Length of the string. 10 by default.
- */
-export default _default;
--- dist/gibstr.module.js
+++
@@ -1,2 +0,0 @@
-var r="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";export default function(t){void 0===t&&(t=10);for(var a="",o=0;o<t;o++)a+=r.charAt(Math.floor(Math.random()*r.length));return a}
-//# sourceMappingURL=gibstr.module.js.map
